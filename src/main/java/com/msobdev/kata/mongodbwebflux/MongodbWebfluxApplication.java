be5b53e181e1948d374efa02f55ffe43749b3993
sobolm01 <Michal.Sobolewski@stepstone.com>
2020-10-03 17:39:30 +0200

    Initial commit

---
+++ src/main/java/com/msobdev/kata/mongodbwebflux/MongodbWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.msobdev.kata.mongodbwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msobdev/kata/mongodbwebflux/MongodbWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msobdev.kata.mongodbwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
